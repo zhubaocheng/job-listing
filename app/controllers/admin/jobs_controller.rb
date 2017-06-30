ed1d5aedb031813a0da30fa21f1b17ce8ea9e101
zhubaocheng <imzhubaocheng@gmail.com>
2017-06-30 17:31:24 +0800

    finished 6-2 step5

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,59 @@
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  before_action :require_is_admin
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def cretae
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path
+      flash[:notice] = " 修改陈功！！！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path
+    flash[:alert] = "删除成功！！！"
+  end
+
+  def require_is_admin
+    if !current_user.admin?
+      redirect_to root_path
+      flash[:alert] = "你不是管理员！没有权限！"
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
