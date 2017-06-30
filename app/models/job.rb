143d36135bcbe3c35ada2493d5cf493b820968eb
zhubaocheng <imzhubaocheng@gmail.com>
2017-06-30 22:36:34 +0800

    implement wage part

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,6 @@
 class Job < ApplicationRecord
   validates_presence_of :title, :description
+  validates_presence_of :wage_upper_bound
+  validates_presence_of :wage_lower_bound
+  validates :wage_lower_bound, numericality: { greater_than: 0}
 end
