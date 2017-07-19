3b44d569e28336a3bf9374a5a4d8697911fedfca
zhubaocheng <imzhubaocheng@gmail.com>
2017-07-20 01:14:23 +0800

    finished search function1

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'simple_form'
 gem 'font-awesome-rails'
 gem 'carrierwave'
 gem 'will_paginate'
+gem 'ransack'
+gem 'will_paginate-bootstrap'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :validate_search_key, only: [:search]
 
   def index
     @jobs = case params[:order]
@@ -53,6 +54,27 @@ class JobsController < ApplicationController
     @job.destroy
     redirect_to jobs_path
     flash[:alert] = "你已成功删除职位！！！"
+  end
+
+  def search
+    if @query_string.present?
+      # 显示符合关键字的公开职位 #
+      search_result = Job.ransack(@search_criteria).result(:distinct => true)
+      @jobs = search_result.published.paginate(:page => params[:page], :per_page => 10 )
+    end  
+  end
+
+  protected
+
+  def validate_search_key
+    # 去除特殊字符 #
+    @query_string = params[:keyword].gsub(/\\|\'|\/|\?/, "") if params[:keyword].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+  def search_criteria(query_string)
+    # 筛选多个栏位 #
+    { :title_or_company_or_city_cont => query_string }
   end
 
   private
