304574e3b1dc58220ef1500aa94bc520e527454e
zhubaocheng <imzhubaocheng@gmail.com>
2017-07-20 15:49:31 +0800

    modify seed

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
   has_many :resumes
-  validates_presence_of :title, :description
+  validates_presence_of :title, :description, :company, :city, :experience, :contact_email
   validates_presence_of :wage_upper_bound
   validates_presence_of :wage_lower_bound
   validates :wage_lower_bound, numericality: { greater_than: 0}
