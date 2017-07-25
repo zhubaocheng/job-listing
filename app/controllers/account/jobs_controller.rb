453f11bce2b1b801a54095925f01676dcb9dc060
zhubaocheng <imzhubaocheng@gmail.com>
2017-07-25 17:30:50 +0800

    modify admin#index

--- app/controllers/account/jobs_controller.rb
+++ app/controllers/account/jobs_controller.rb
@@ -2,6 +2,6 @@ class Account::JobsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @jobs = current_user.collected_jobs.paginate(:page => params[:page], :per_page => 5)
+    @jobs = current_user.collected_jobs.paginate(:page => params[:page], :per_page =>4)
   end
 end
