f550f562c9869b4fec5899ec658b999655ba02ef
zhubaocheng <imzhubaocheng@gmail.com>
2017-07-25 10:15:35 +0800

    implement collect fundation

---
+++ app/controllers/account/jobs_controller.rb
@@ -0,0 +1,7 @@
+class Account::JobsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @jobs = current_user.collected_jobs.paginate(:page => params[:page], :per_page => 5)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
+  namespace :account do
+    resources :jobs
+  end
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
