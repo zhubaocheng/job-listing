c1ffa4ebaa6b60277f6f4c338bc5341a8855ac28
zhubaocheng <imzhubaocheng@gmail.com>
2017-07-26 17:48:42 +0800

    add username & common user/admin user to devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
   # GET /resource/sign_up
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-  :sessions      => "users/sessions",
-  :registrations => "users/registrations",
-  :passwords     => "users/passwords",
-}
+  devise_for :users, :controllers => {  :registrations => "users/registrations" }
   namespace :admin do
     resources :jobs do
       member do
